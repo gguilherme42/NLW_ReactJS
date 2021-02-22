e2a23ac082141d5fbdd7924259d8ed43f78254cc
Guilherme Rosa <grosa@gvdasa.com.br>
2021-02-22 13:07:18 -0300

    NLW#4 - Rumo ao próximo nível [ReactJS]

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+
+export function ExperienceBar() {
+
+    const [experience, setExperience] = useState(0)
+
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{width: experience }}></div>
+                <span className="current-experience" style={{ left: experience }}>{experience} xp</span>
+            </div>
+            
+            <span>600 xp</span>
+            <main>
+                <div>
+                    <button onClick={() => setExperience((experience + 50) % 600)}>Aumentar xp</button>
+                </div>
+            </main> 
+        </header>
+    );
+}
